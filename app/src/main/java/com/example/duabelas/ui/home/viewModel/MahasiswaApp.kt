ebfda936acc9ddb04b43690d1dc36d29cf0e7a03
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2024-12-29 22:57:49 +0700

    revisi isi MahasiswaApp

--- app/src/main/java/com/example/duabelas/ui/home/viewModel/MahasiswaApp.kt
+++ app/src/main/java/com/example/duabelas/ui/home/viewModel/MahasiswaApp.kt
@@ -1,14 +1,30 @@
 package com.example.duabelas.ui.home.viewModel
 
-import android.app.Application
-import com.example.duabelas.ui.AppContainer
-import com.example.duabelas.ui.MahasiswaContainer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import com.example.duabelas.navigation.PengelolaHalaman
 
-class MahasiswaApplications: Application() {
-    lateinit var container: AppContainer
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MahasiswaApp(modifier: Modifier = Modifier){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+    ){
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ){
+            PengelolaHalaman()
+        }
 
-    override fun onCreate() {
-        super.onCreate()
-        container = MahasiswaContainer()
     }
 }
