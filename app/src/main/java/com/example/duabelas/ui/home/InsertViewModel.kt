5a0dbb7afb7d746ce15a81739db3a93d4a285516
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2024-12-29 20:43:44 +0700

    membuat class InsertViewModel

---
+++ app/src/main/java/com/example/duabelas/ui/home/InsertViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.duabelas.ui.home
+
+import androidx.lifecycle.ViewModel
+import com.example.duabelas.repository.MahasiswaRepository
+
+class InsertViewModel(private val mhs: MahasiswaRepository) : ViewModel(){
+
+
+}
