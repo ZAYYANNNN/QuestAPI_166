4baa560233df46ff5f89e5a56a9417017e0db085
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2024-12-29 20:48:56 +0700

    membuat fungsi inseruievent dan dataclass insertuistate dan insertuievent

--- app/src/main/java/com/example/duabelas/ui/home/InsertViewModel.kt
+++ app/src/main/java/com/example/duabelas/ui/home/InsertViewModel.kt
@@ -1,9 +1,45 @@
 package com.example.duabelas.ui.home
 
 import androidx.lifecycle.ViewModel
+import com.example.duabelas.model.Mahasiswa
 import com.example.duabelas.repository.MahasiswaRepository
 
 class InsertViewModel(private val mhs: MahasiswaRepository) : ViewModel(){
 
 
 }
+
+data class InsertUiState(
+    val insertUiEvent:InsertUiEvent = InsertUiEvent()
+)
+
+data class InsertUiEvent(
+    val nim:String = "",
+    val nama:String = "",
+    val alamat:String = "",
+    val jenisKelamin:String = "",
+    val kelas:String = "",
+    val angkatan:String = "",
+)
+
+fun InsertUiEvent.toMhs():Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jeniskelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+fun  Mahasiswa.toUiStateMhs():InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent()
+)
+
+fun Mahasiswa.toInsertUiEvent():InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jeniskelamin,
+    kelas = kelas,
+    angkatan =angkatan
+)
