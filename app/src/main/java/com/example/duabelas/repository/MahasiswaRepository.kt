1706888d5d273d644f68667436207454ac29a297
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2024-12-29 20:16:59 +0700

    menambahkan insert, update, get dan class networkmahasiswarepository

--- app/src/main/java/com/example/duabelas/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/duabelas/repository/MahasiswaRepository.kt
@@ -1,6 +1,9 @@
 package com.example.duabelas.repository
 
 import com.example.duabelas.model.Mahasiswa
+import com.example.duabelas.service_api.MahasiswaService
+import java.io.IOException
+
 
 interface MahasiswaRepository{
     suspend fun getMahasiswa(): List<Mahasiswa>
@@ -12,4 +15,42 @@ interface MahasiswaRepository{
     suspend fun  deleteMahasiswa(nim: String)
 
     suspend fun getMahasiswabyNim(nim: String): Mahasiswa
+}
+
+class NetworkMahasiswaRepository(
+    private val mahasiswaApiService: MahasiswaService
+) : MahasiswaRepository {
+    override suspend fun getMahasiswa(): List<Mahasiswa> =
+        mahasiswaApiService.getAllMahasiswa()
+
+
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        mahasiswaApiService.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
+        mahasiswaApiService.updateMahasiswa(nim, mahasiswa)
+    }
+
+    override suspend fun deleteMahasiswa(nim: String) {
+        try {
+            val response = mahasiswaApiService.deleteMahasiswa(nim)
+            if (!response.isSuccessful) { // Corrected spelling
+                throw IOException(
+                    "Failed to delete Mahasiswa. HTTP Status Code: " + "${response.code()}"
+                )
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+
+    override suspend fun getMahasiswabyNim(nim: String): Mahasiswa {
+        return mahasiswaApiService.getMahasiswabyNim(nim)
+    }
+
 }
