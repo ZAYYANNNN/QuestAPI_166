b18e262cfae172791d7bc42e474baf84434c7880
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2024-12-29 22:16:26 +0700

    membuat fungsi HomeStatus

---
+++ app/src/main/java/com/example/duabelas/navigation/PengelolaHalaman.kt
@@ -0,0 +1,36 @@
+package com.example.duabelas.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.duabelas.ui.View.DestinasiEntry
+import com.example.duabelas.ui.View.EntryMhsScreen
+
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier,
+    ){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+                onDetailClick = {
+                }
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route){
+                    popUpTo(DestinasiHome.route){
+                        inclusive = true
+                    }
+                }
+            })
+        }
+    }
+}
