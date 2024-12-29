b1c8f099a0fdc4934eb1c7a816108950858a1213
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2024-12-29 20:59:04 +0700

    membuat class HomeViewModel

--- app/src/main/java/com/example/duabelas/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/duabelas/ui/home/HomeViewModel.kt
@@ -1,9 +1,23 @@
 package com.example.duabelas.ui.home
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.duabelas.model.Mahasiswa
+import com.example.duabelas.repository.MahasiswaRepository
 
 sealed class HomeUiState {
     data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
     object  Error : HomeUiState()
     object Loading : HomeUiState()
+}
+
+class HomeViewModel (private val mhs: MahasiswaRepository): ViewModel(){
+    var mhsUIState:HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getMhs()
+    }
 }
