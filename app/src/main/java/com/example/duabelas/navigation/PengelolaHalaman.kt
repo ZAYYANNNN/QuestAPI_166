019f771b0d43b8540f1667db44b7873df7be6018
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2024-12-29 22:21:52 +0700

    import destinasi home

--- app/src/main/java/com/example/duabelas/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/duabelas/navigation/PengelolaHalaman.kt
@@ -7,7 +7,9 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.duabelas.ui.View.DestinasiEntry
+import com.example.duabelas.ui.View.DestinasiHome
 import com.example.duabelas.ui.View.EntryMhsScreen
+import com.example.duabelas.ui.View.HomeScreen
 
 @Composable
 fun PengelolaHalaman(navController: NavHostController = rememberNavController()){
